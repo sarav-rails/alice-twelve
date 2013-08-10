b7999d104276dd7d860965f2fea59f57a8fd8a69
Saravanan K <saravani20@gmail.com>
2013-08-10 08:44:03 +0530

    initial spree app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AliceTwelve::Application.config.secret_token = '289e64c5664f4b5bd82192809c0756d8e2b95d01e2eba4844015821128357c14268a4fa76a0c5b1fe0f26a23785603c00974175532d14f122cbf1a9dd9bd3018'
